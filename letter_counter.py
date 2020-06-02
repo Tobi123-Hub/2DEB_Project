a8b3fd680a31b68bd7407a7a45b852369c2108ff
Tobi123_Hub <tobben911@hotmail.com>
2020-06-02 11:02:03 +0200

    Nye greier

--- letter_counter.py
+++ letter_counter.py
@@ -1,19 +1,42 @@
+import matplotlib.pyplot as plt
+
 f = open("input.txt","r") #Åpna fila vi skal telle bokstaver i
 
 f_out = open("output.txt","w") #Åpne utskriftsfil for å lagre data
 
 file_content = f.read() #Lagre innhold i fila til file_content som en string
 
-count = 0
+
+vokal_array = ['a', 'e', 'i', 'o', 'u', 'y']
+n = len(vokal_array)
+telle_array = [0]*n
+
+
+
 
 for i in file_content:
-    if (i == 'a') or (i == 'A') or (i == 'e') or (i == 'E') or (i == 'i') or (i == 'I') or (i == 'o') or (i == 'O') or (i == 'u') or (i == 'U') or (i == 'y') or (i == 'Y'):
-        count = count + 1
-        
-print(count)
+    index = 0
+    for k in vokal_array:
+        if (i.lower() == k):
+            telle_array[index] = telle_array[index] + 1
+        index = index + 1
 
-f_out.write("A or a: ") #Skriv til output.txt om hvor mange store og små A'er vi fant
-f_out.write(str(count))
+
+index = 0
+for k in vokal_array:
+    f_out.write(k) #Skriv til output.txt om hvor mange store og små A'er vi fant
+    f_out.write(": ")
+    f_out.write(str(telle_array[index]))
+    f_out.write("\n")
+    index = index + 1
+    
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+langs = ['a', 'e', 'i', 'o', 'u', 'y']
+vokaler = [telle_array[index]]
+ax.bar(langs,vokaler)
+plt.show()
+
 
 f.close() #Lukka fila som vi skal telle i
 f_out.close()
